17d3fc59c66d0dc043c2690272feee962020496a
Milan Felix Šulc <rkfelix@gmail.com>
2023-07-27 09:20:02 +0200

    Bootstrap: refactor

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,16 +1,55 @@
 <?php declare(strict_types = 1);
 
+namespace App;
+
 use Contributte\Bootstrap\ExtraConfigurator;
+use Nette\DI\Compiler;
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Tracy\Debugger;
 
-require __DIR__ . '/../vendor/autoload.php';
+final class Bootstrap
+{
 
-$configurator = new ExtraConfigurator();
+	public static function boot(): ExtraConfigurator
+	{
+		$configurator = new ExtraConfigurator();
+		$configurator->setTempDirectory(__DIR__ . '/../var/tmp');
 
-$configurator->setDebugMode(true);
-$configurator->enableDebugger(__DIR__ . '/../var/log');
-$configurator->setTempDirectory(__DIR__ . '/../var/tmp');
+		// @phpstan-ignore-next-line
+		$configurator->onCompile[] = function (ExtraConfigurator $configurator, Compiler $compiler): void {
+			// Add env variables to config structure
+			$compiler->addConfig(['parameters' => $configurator->getEnvironmentParameters()]);
+		};
 
-$configurator->addConfig(__DIR__ . '/../config/config.neon');
-$configurator->addConfig(__DIR__ . '/../config/local.neon');
+		// According to NETTE_DEBUG env
+		$configurator->setEnvDebugMode();
 
-return $configurator->createContainer();
+		// Enable tracy and configure it
+		$configurator->enableTracy(__DIR__ . '/../var/log');
+		Debugger::$errorTemplate = __DIR__ . '/../resources/tracy/500.phtml';
+
+		// Provide some parameters
+		$configurator->addStaticParameters([
+			'rootDir' => realpath(__DIR__ . '/..'),
+			'appDir' => __DIR__,
+			'wwwDir' => realpath(__DIR__ . '/../www'),
+		]);
+
+		$configurator->addConfig(__DIR__ . '/../config/config.neon');
+		$configurator->addConfig(__DIR__ . '/../config/local.neon');
+
+		return $configurator;
+	}
+
+	public static function runCli(): void
+	{
+		self::boot()
+			->addStaticParameters([
+				'scope' => 'cli',
+			])
+			->createContainer()
+			->getByType(SymfonyApplication::class)
+			->run();
+	}
+
+}
