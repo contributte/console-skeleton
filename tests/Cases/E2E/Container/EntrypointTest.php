c44cb65be6f3814859ae30712b402a87802f995c
Milan Felix Šulc <rkfelix@gmail.com>
2023-07-27 09:20:17 +0200

    Tests: introduction

---
+++ tests/Cases/E2E/Container/EntrypointTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Cases\E2E\Container;
+
+use App\Bootstrap;
+use Symfony\Component\Console\Application as CliApplication;
+use Contributte\Utils\FileSystem;
+use Nette\DI\Container;
+use Tester\Assert;
+use Tester\TestCase;
+use Tests\Toolkit\Tests;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+final class EntrypointTest extends TestCase
+{
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		if (!file_exists(Tests::ROOT_PATH . '/config/local.neon')) {
+			FileSystem::copy(
+				Tests::ROOT_PATH . '/config/local.neon.example',
+				Tests::ROOT_PATH . '/config/local.neon'
+			);
+		}
+	}
+
+	public function testCli(): void
+	{
+		$container = Bootstrap::boot()->createContainer();
+		$container->getByType(CliApplication::class);
+
+		Assert::type(Container::class, $container);
+	}
+
+}
+
+(new EntrypointTest())->run();
